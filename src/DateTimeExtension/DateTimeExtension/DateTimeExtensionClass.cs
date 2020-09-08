f237637e372bb45cf78194a886496c4de9aa93da
Nicola <zortea@gmail.com>
2020-09-08 09:58:21 +0200

    Initial Commit

---
+++ src/DateTimeExtension/DateTimeExtension/DateTimeExtensionClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DateTimeExtension
+{
+    public static class DateTimeExtensionClass
+    {
+        public static DateTime StartOfDay(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, 0);
+        }
+        public static DateTime EndOfDay(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, 23, 59, 59, 999);
+        }
+
+        public static DateTime StartOfMonth(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, 1);
+        }
+
+        public static DateTime EndOfMonth(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, 1).AddMonths(1).Subtract(new TimeSpan(0, 0, 0, 0, 1));
+        }
+    }
+}
---
+++ src/DateTimeExtension/DateTimeExtension_Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DateTimeExtension;
+
+namespace DateTimeExtension_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            
+            Console.WriteLine("StartOfDay()");
+            Console.WriteLine(now.StartOfDay());
+
+            Console.WriteLine("EndOfDay()");
+            Console.WriteLine(now.EndOfDay());
+
+            Console.WriteLine("StartOfMonth()");
+            Console.WriteLine(now.StartOfMonth());
+                  
+            Console.WriteLine("EndOfMonth()");
+            Console.WriteLine(now.EndOfMonth());
+
+            Console.ReadLine();
+
+        }
+    }
+}
